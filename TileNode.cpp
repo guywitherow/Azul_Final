fb7cf06a67010179fcdc81f54274e615861bce8c
Guy Witherow s3783428 <s3783428@student.rmit.edu.au>
2020-06-04 16:09:14 +1000

    clean up

--- TileNode.cpp
+++ TileNode.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
 #include "TileNode.h"
 
-TileNode::TileNode(Tile *tileData, TileNode *nextTileNode)
-{
-    this->tileData = tileData;
-    this->nextTileNode = nextTileNode;
+TileNode::TileNode(Tile* tileData, TileNode* nextTileNode) {
+	this->tileData = tileData;
+	this->nextTileNode = nextTileNode;
 }
 
-TileNode::~TileNode()
-{
-   delete tileData;
-   //dont delete next tile node, 
-   //that will cause chain deletion of all tiles down the line
+TileNode::~TileNode() {
+	delete tileData;
+	//dont delete next tile node,
+	//that will cause chain deletion of all tiles down the line
 }
 
-Tile *TileNode::getTileData()
-{
-    return this->tileData;
+Tile* TileNode::getTileData() {
+	return this->tileData;
 }
 
-TileNode *TileNode::getNextTileNode()
-{
-    return this->nextTileNode;
+TileNode* TileNode::getNextTileNode() {
+	return this->nextTileNode;
 }
 
-void TileNode::setNextTileNode(TileNode *nextTileNode)
-{
-    this->nextTileNode = nextTileNode;
+void TileNode::setNextTileNode(TileNode* nextTileNode) {
+	this->nextTileNode = nextTileNode;
 }
 
 // We can use this to get a tile tostring once we've set up tile.cpp class
 std::string TileNode::getTileDataToString(Tile* tileData) {
-    std::string statement = tileData->tileToString();
-    return statement;
+	std::string statement = tileData->tileToString();
+	return statement;
 }
